d85d6540d07345bab6da0d8fa40b6d6c54445755
ferriccc <shrey.s4@ahduni.edu.in>
2024-07-03 19:18:45 +0530

    UPD
    Former-commit-id: 9fd077a65fc1be71ca088151e1775d5abb8b7a31

--- home/shrey/scripts/shot.sh
+++ home/shrey/scripts/shot.sh
@@ -1,6 +1,5 @@
-# for some reason using /bin/bash was not working and fish worked!
-
 #!/usr/bin/fish
+# for some reason using /bin/bash was not working and fish worked!
 
 grim -g "$(slurp)" $HOME/screenshots/last.png
 wl-copy < $HOME/screenshots/last.png
