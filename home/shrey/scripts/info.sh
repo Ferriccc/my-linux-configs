b6690692e73172e1d332719f12dabc9ed5531805
ferriccc <shrey.s4@ahduni.edu.in>
2024-11-05 19:54:02 +0530

    last bye

--- home/shrey/scripts/info.sh
+++ home/shrey/scripts/info.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
 info=$(date +"%I:%M%p, %A %d %B")
+bat=$(upower -i /org/freedesktop/UPower/devices/battery_BAT0 | grep percentage | awk '{ print $2 }')
+uptime=$(uptime -p)
 
-notify-send "$info"
+notify-send "System Info" "$info
+Battery: $bat
+Uptime: $uptime"
